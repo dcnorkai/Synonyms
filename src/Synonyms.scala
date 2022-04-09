d23062230d3699336b0890b213167aab8f74e20f
dcnorkai <54694121+dcnorkai@users.noreply.github.com>
2022-04-08 20:24:34 -0400

    Still needs print statements to output file

--- src/Synonyms.scala
+++ src/Synonyms.scala
@@ -4,46 +4,41 @@ import java.io.File         //technically not needed
 import java.io.FileWriter   //makes it easy to append
 import java.io.PrintWriter  //makes "print" and "println" available
 
-object Synonyms extends App {
-  class Identifier {
-    var key = ""
-    var synList = ""
-  }
+//BUG: Not reading multiple lines in for synonyms
+//Need to convert all synonyms to lowercase
 
-  //val map: Map[String, Set[String]] = Map()
-  //map.keys
-  //map("recal") -> Set{}
+object Synonyms extends App {
+
   val lines: List[String] = Source.fromFile("./resources/Fallows1898.txt").getLines.toList
-  val selectedLines = lines.drop(99)
+  val dictionary = lines.slice(99, 33712)
   var myMap: Map[String, List[String]] = Map()
 
-  //val filep = new PrintWriter(new FileWriter("output.txt"));  //appends
-  //filep.println(selectedLines)
-  //filep.close()
-
-  // Make regex for KEY, SYN, ANT
   val KEY_pattern = "KEY: (.*)".r
   val SYN_pattern = "SYN: (.*)".r
   val ANT_pattern = "ANT: (.*)".r
   val character_regex = "([a-zA-Z]*).*".r
   var SYN_flag = false
   var key: String = ""
-  for (line <- selectedLines) {
+
+  //Looping through dictionary
+  for (line <- dictionary) {
     line match {
       case KEY_pattern(c) => {
-        println("Test")
-        val key: String = c.substring(0, c.length() - 1)
+        key = (c.substring(0, c.length() - 1)).toLowerCase //Removes period at the end of the key string
+        //println("KEY: " + key) //Print test for key
         if (!(myMap.contains(key))) {
-          myMap += (key, List())
+          myMap += (key -> List())
         }
       }
       case SYN_pattern(c) => {
         SYN_flag = true
-        val split_words: List[String] = (c.split(", ").map(x => {
+        val split_words: List[String] = (c.toLowerCase.split(", ").map(x => {
           x match {
             case character_regex(c2) => c2
           }
         })).toList
+        //println("SYN: " + split_words) //Print test for split_words
+        //println(myMap(key))
         myMap += (key -> List.concat(myMap(key), split_words))
       }
       case ANT_pattern(c) => {
@@ -54,47 +49,35 @@ object Synonyms extends App {
       }
       case _ => {
         if (SYN_flag) {
-          //val split_words: List[String] = (_.split(", ").map(x => {x match {case character_regex(c2) => c2}})).toList
-          //myMap += (key -> List.concat(myMap(key), split_words)
+          //println("TEST: " + line)
+          val split_words: List[String] = (line.toLowerCase.split(", ").map(x => {x match {case character_regex(c2) => c2}})).toList
+          //println(split_words)
+          myMap += (key -> List.concat(myMap(key), split_words))
+        }
+      }
+    }
+  }
+
+  //Prints myMap
+  //for ((k,s) <- myMap) printf("KEY: %s, SYN: %s\n", k, s)
+
+  //Looping through myMap
+  for ((k,synList) <- myMap) {
+    //Check if first letter of key is q
+    if (k(0) == 'q') {
+      //For each value associated with the current key
+      for (s <- synList) {
+        if (myMap.contains(s)) {
+          if (myMap(s).length > 0) {
+            if (myMap(s).contains(k)) {
+              println(k + " and " + s + " are reciprocal synonyms")
+            }
+            else {
+              println(k + " lists "+ s + " but " + s + " has a list of words without " + k)
+            }
+          }
         }
       }
     }
   }
 }
-
-
-
-/*  object MyClass {
-    def main(args: Array[String]) {
-      var A: Map[String, List[String]] = Map()
-      A += ("Hello" -> List("World"))
-      val ls: List[String] = A("Hello")
-      A += ("Hello" -> List.concat(ls, List("Foo")))
-      println(A)
-    }
-  }*/
-
-
-/*
-import io.StdIn._
-import io.Source
-import java.io.File         //technically not needed
-import java.io.FileWriter   //makes it easy to append
-import java.io.PrintWriter  //makes "print" and "println" available
-
-object Synonyms extends App {
-  class Identifier {
-    var key = ""
-    var synList = ""
-  }
-  val lines: List[String] = Source.fromFile("./resources/Fallows1898.txt").getLines.toList
-
-  val selectedLines = lines.drop(99)
-
-  // check the value of lines(100)
-  val line1 = selectedLines(0)
-  println(line1)
-
-  // lines.slice(100, lines.length)
-
-}*/
